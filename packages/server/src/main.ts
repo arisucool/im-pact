3fc16e5ee8c6e20a7df94f59629fff0f8afa5b22
mugip <strawberry@arisu.cool>
2020-12-24 01:36:10 +0900

    Add: server - Swagger による OpenAPI の対応

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -7,6 +8,35 @@ async function bootstrap() {
   // URLのプレフィックスが /api となるように設定
   app.setGlobalPrefix('api');
 
+  // アプリケーション名の取得
+  let package_name = null, package_version = null;
+  try {
+    const package_json = require(`${__dirname}/../../../package.json`);
+    package_name = package_json.name;
+    package_version = package_version = package_json.version;
+  } catch (e) {}
+  
+  // ドキュメント情報の設定
+  let doc_app_name = null, doc_app_description = null, doc_app_version = 'x.x.x';
+  if (!package_name || package_name === 'im-pact') {
+    doc_app_name = 'im pact';
+    doc_app_description = `for "${doc_app_name}" by arisu.cool 🍓 Project.`;
+  } else {
+    doc_app_name = package_name;
+    doc_app_description = `for ${doc_app_name} (based on "im pact" by arisu.cool 🍓 Project).`;
+  }
+  if (package_version) doc_app_version = package_version;
+
+  // Swagger による OpenAPI の対応 (/api/docs/ にてドキュメントを公開)
+  const options = new DocumentBuilder()
+    .setTitle(`${doc_app_name} API Document`)
+    .setDescription(`API Document ${doc_app_description}`)
+    .setVersion(doc_app_version)
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api/docs', app, document);
+
+  // サーバの待ち受けを開始
   await app.listen(3000);
 }
 bootstrap();

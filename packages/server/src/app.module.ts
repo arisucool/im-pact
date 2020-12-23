15b705067a6b4c8a9a731386f3492e21b2aeb7f8
mugip <strawberry@arisu.cool>
2020-12-24 03:13:04 +0900

    Add: 本番環境用のビルドを可能に (npm run build の対応)

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
 @Module({
-  imports: [],
+  imports: [
+    // 本番環境にて Angular アプリケーション (../../client/dist/client/) を静的ファイルとしてサーブするための設定
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'client', 'dist', 'client'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

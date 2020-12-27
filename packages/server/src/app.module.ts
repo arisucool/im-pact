1702fc1fd78fab66fb0d2a5832ef7eb6e4a9a342
mugip <strawberry@arisu.cool>
2020-12-27 11:12:31 +0900

    Add: server - TypeORM によるデータベース対応

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -5,8 +5,17 @@ import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
   imports: [
+    // データベースへ接続するための設定
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true
+    }),
     // 本番環境にて Angular アプリケーション (../../client/dist/client/) を静的ファイルとしてサーブするための設定
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', '..', 'client', 'dist', 'client'),
